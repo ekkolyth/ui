d616c63c2ece374b14a7bd6d1400d1c403647799
Mike Kenway <mike@mikekenway.com>
2026-01-15 20:14:34 -0500

    reorganized codebase

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,59 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import js from "@eslint/js";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import storybookPlugin from "eslint-plugin-storybook";
+import tseslint from "typescript-eslint";
+import globals from "globals";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+export default [
+    // Base recommended configs
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
 
-export default eslintConfig;
+    // React config
+    {
+        files: ["**/*.{ts,tsx,js,jsx}"],
+        plugins: {
+            react: reactPlugin,
+            "react-hooks": reactHooksPlugin,
+        },
+        languageOptions: {
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.es2021,
+            },
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        rules: {
+            ...reactPlugin.configs.recommended.rules,
+            ...reactHooksPlugin.configs.recommended.rules,
+            "react/react-in-jsx-scope": "off", // Not needed in React 19
+            "react/prop-types": "off", // Using TypeScript
+        },
+    },
+
+    // Storybook config
+    ...storybookPlugin.configs["flat/recommended"],
+
+    // Ignores
+    {
+        ignores: [
+            "node_modules/**",
+            ".next/**",
+            "out/**",
+            "build/**",
+            "storybook-static/**",
+            ".storybook/storybook-static/**",
+            "next-env.d.ts",
+        ],
+    },
+];
